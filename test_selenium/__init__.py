531b2e90e789378415d3da10500c4e1d5aee7c38
indeyo <indeyo@git.com>
2020-01-07 08:35:02 +0800

    finish selenium homework_1

---
+++ test_selenium/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+"""
+@Project : Hogwarts_11
+@File    : __init__.py.py
+@Time    : 2019-12-29  11:34:31
+@Author  : indeyo_lin
+@Version : 
+@Remark  :
+"""
