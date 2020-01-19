0381ddf941f08d72c8e6612e4b7145319fff0b7b
indeyo <indeyo@git.com>
2020-01-19 11:09:53 +0800

    finish selenium homework3

---
+++ test_selenium/test_wework_homework.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+"""
+@Project : Hogwarts_11
+@File    : test_wework_homework.py
+@Time    : 2020-01-15  07:44:10
+@Author  : indeyo_lin
+@Version : 
+@Remark  :
+"""
